ed21581fad2f5263e4b5b003ab2bb9e840fd5bb9
erick <erick@DESKTOP-EUVT7JU>
2017-07-18 12:37:25 -0600

    Conexion

---
+++ src/conexion/conectar.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package conexion;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author erick
+ */
+public class conectar {
+
+    Connection conexion = null;
+
+    public Connection conexion() {
+        try {
+            conexion = null;
+            Class.forName("com.mysql.jdbc.Driver");
+            conexion = DriverManager.getConnection("jdbc:mysql://127.0.0.1/conexion", "root", "12");
+            System.out.println("conexion establecida");
+        } catch (ClassNotFoundException | SQLException e) {
+            System.out.println("error de conexion");
+            JOptionPane.showMessageDialog(null, "error de conexion " + e);
+        }
+        return conexion;
+    }
+}
